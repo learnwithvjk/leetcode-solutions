40b361d7f683e7d27e42710836b947e123c40dd0
vijayakumar boopathy <learnwithvjk@gmail.com>
2023-08-31 23:22:04 +0530

    feat: leet code 2423 added remove letter to equalize frequency

---
+++ problem-list/leet_code_2423/python_solutions/Sol_1.py
@@ -0,0 +1,48 @@
+class Solution(object):
+    def equalFrequency(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        counter = {}
+        for char in word:
+            try:
+                counter[char] += 1
+            except:
+                counter[char] = 1
+        tempDict = {}
+        tempKey = None
+        for key in counter:
+            tempKey = counter[key]
+            try:
+                tempDict[counter[key]].append(key)
+            except:
+                tempDict[counter[key]] = [key]
+
+        # if only one char with single freq then it can be removed
+        # if only one
+        if len(tempDict) == 1:
+            try:
+                if len(tempDict[tempKey]) == 1 or tempKey == 1:
+                    return True
+            except:
+                return False
+
+        if len(tempDict) == 2:
+            minCount = 101  # since word count wont exceed 100
+            maxCount = -1  # since count cant be negative
+            for key in tempDict:
+                minCount = min(minCount, key)
+                maxCount = max(maxCount, key)
+
+            if minCount == 1 and len(tempDict[minCount]) == 1:
+                return True
+
+            if maxCount - minCount == 1 and len(tempDict[maxCount]) == 1:
+                return True
+
+        return False
+
+
+def getExecutable(*args):
+    return Solution().equalFrequency(*args)
